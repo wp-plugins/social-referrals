dc98fef78f155f7f7dcd10d87b37b45060f21e20
dkukral <dkukral@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 17:45:29 +0000

    verified compat with wp 3.9.1
    git-svn-id: https://plugins.svn.wordpress.org/social-referrals/trunk@916674 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Social Referrals ===
-Contributors: jboydston, dkukral, Droyal
+Contributors: jboydston, dkukral
 Tags: social, facebook, twitter, statistics
 Requires at least: 3.0
 Tested up to: 3.3.1
-Stable Tag: 1.2
+Stable Tag: 1.4
 
 Plugin for WordPress logs social site referrals and provides shortcodes for displaying them.
 
@@ -12,7 +12,7 @@ Plugin for WordPress logs social site referrals and provides shortcodes for disp
 Logs and displays social site referrals
 
 == License ==
-Copyright 2010 - 2012 Joe Boydston, Don Kukral
+Copyright 2010 - 2014 Joe Boydston, Don Kukral
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -30,14 +30,14 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 == Installation ==
 
-Copy "social-referrals" folder to your WordPress Plugins Directory. 
+Copy "social-referrals" folder to your WordPress Plugins Directory.
 Activate plugin via WordPress settings.
 
 == Other Notes ==
 
 No shortcodes yet .. but it does keep track of the number of referrals in the last 24 hours.
 
-Two WP options are set with these values and updated whenever a new referrer is logged. 
+Two WP options are set with these values and updated whenever a new referrer is logged.
 
 social_referrals_facebook_count
 social_referrals_twitter_count
@@ -47,14 +47,19 @@ social_referrals_twitter_count
 None.
 
 == Changelog ==
+= 1.4 = 
+Verified compat with WP 3.9.1 
 
-= 1.2 = 
+= 1.3 =
+Added wordpress.com tracker
+
+= 1.2 =
 Fixed hardcoded table name
 
-= 1.1 = 
+= 1.1 =
 Added setting to custom how many hours back to display of the count.
 
-= 1.0 = 
+= 1.0 =
 Initial version
 
 == Frequently Asked Questions ==
--- social-referrals.php
+++ social-referrals.php
@@ -3,7 +3,7 @@
 Plugin Name: Social Referrals
 Plugin URI: http://yourdomain.com/
 Description: Logs and displays social site referrals
-Version: 1.2
+Version: 1.4
 Author: Don Kukral
 Author URI: http://yourdomain.com
 License: GPL
@@ -18,7 +18,7 @@ function social_referrals_init() {
      if (get_option('social_referrals_twitter', 0)) {
          $ref_url = parse_url($_SERVER['HTTP_REFERER']);
          if ($ref_url['host'] == 't.co') {
-             social_referrals_log_entry($post->ID, $_SERVER['HTTP_REFERER'], 0, 1);
+             social_referrals_log_entry($post->ID, $_SERVER['HTTP_REFERER'], 0, 1, 0);
              return;
          }
      }
@@ -26,42 +26,53 @@ function social_referrals_init() {
     if (get_option('social_referrals_facebook', 0)) {
         $ref_url = parse_url($_SERVER['HTTP_REFERER']);
          if (($ref_url['host'] == 'www.facebook.com') || ($ref_url['host'] == 'm.facebook.com')) {
-             social_referrals_log_entry($post->ID, $_SERVER['HTTP_REFERER'], 1, 0);
+             social_referrals_log_entry($post->ID, $_SERVER['HTTP_REFERER'], 1, 0, 0);
+             return;
+         }
+     }
+
+    if (get_option('social_referrals_wordpress', 0)) {
+        $ref_url = parse_url($_SERVER['HTTP_REFERER']);
+         if (($ref_url['host'] == 'www.wordpress.com')) {
+             social_referrals_log_entry($post->ID, $_SERVER['HTTP_REFERER'], 1, 0, 0);
              return;
          }
      }
 }
 
-function social_referrals_log_entry($post_id, $referral_url, $facebook, $twitter) {
+function social_referrals_log_entry($post_id, $referral_url, $facebook, $twitter, $wordpress) {
     global $wpdb;
     $table_name = $wpdb->prefix . "social_referrals";
     $wpdb->insert(
-        $table_name, 
+        $table_name,
         array(
             'post_id' => $post_id,
             'referral_url' => $referral_url,
             'facebook' => $facebook,
-            'twitter' => $twitter
+            'twitter' => $twitter,
+            'wordpress' => $wordpress
         ),
         array(
             '%d',
             '%s',
             '%d',
+            '%d',
             '%d'
         )
     );
-    $row = $wpdb->get_row("SELECT SUM(facebook) AS facebook, SUM(twitter) AS twitter FROM " . $wpdb->prefix . "social_referrals WHERE ts > NOW() - INTERVAL " . get_option('social_referrals_hours', 24) . " HOUR");
+    $row = $wpdb->get_row("SELECT SUM(facebook) AS facebook, SUM(twitter) AS twitter, SUM(wordpress) as wordpress FROM " . $wpdb->prefix . "social_referrals WHERE ts > NOW() - INTERVAL " . get_option('social_referrals_hours', 24) . " HOUR");
     update_option('social_referrals_facebook_count', $row->facebook);
     update_option('social_referrals_twitter_count', $row->twitter);
-    
+    update_option('social_referrals_wordpress_count', $row->wordpress);
+
 }
 
 function social_referrals_admin_menu() {
     add_options_page(
-        'Social Referrals', 
-        'Social Referrals', 
+        'Social Referrals',
+        'Social Referrals',
         'administrator',
-        'social_referrals', 
+        'social_referrals',
         'social_referrals_settings_page');
 }
 
@@ -71,6 +82,8 @@ function social_referrals_settings_page() {
         else { delete_option('social_referrals_facebook'); }
         if ($_POST['social_referrals_twitter']) { update_option('social_referrals_twitter', 1); }
         else { delete_option('social_referrals_twitter'); }
+        if ($_POST['social_referrals_wordpress']) { update_option('social_referrals_wordpress', 1); }
+        else { delete_option('social_referrals_wordpress'); }
         if ($_POST['social_referrals_hours']) { update_option('social_referrals_hours', $_POST['social_referrals_hours']); }
         else { update_option('social_referrals_hours', '24'); }
         echo '<div class="updated"><p>Social Referrals Settings Updated</p></div>';
@@ -83,7 +96,7 @@ function social_referrals_settings_page() {
 	        <form method="post" action="" id="email_on_publish_form">
 	        <input type="hidden" name="action" value="update" />
 	        <?php wp_nonce_field('update-options'); ?>
-	        
+
 	        <table class="form-table">
 	        <tr>
 	        <td><input type="checkbox" name="social_referrals_facebook" <?php echo checked(get_option('social_referrals_facebook'), 1); ?>/> Track Facebook referrals.</td>
@@ -91,6 +104,9 @@ function social_referrals_settings_page() {
 	        <tr>
 	        <td><input type="checkbox" name="social_referrals_twitter" <?php echo checked(get_option('social_referrals_twitter'), 1); ?>/> Track Twitter referrals.</td>
 	        </tr>
+            <tr>
+            <td><input type="checkbox" name="social_referrals_wordpress" <?php echo checked(get_option('social_referrals_wordpress'), 1); ?>/> Track Wordpress.com referrals.</td>
+            </tr>
 	        <tr>
 	        <tr>
 	        <td>Display referrals for last <input type="text" name="social_referrals_hours" value="<?php echo get_option('social_referrals_hours', 24); ?>" size="3" style="text-align: center;"/> hours.</td>
@@ -109,9 +125,9 @@ function social_referrals_settings_page() {
 function social_referrals_install() {
     global $wpdb;
     global $social_referrals_db_ver;
-    
+
     $table_name = $wpdb->prefix . "social_referrals";
-    
+
     $sql = "CREATE TABLE " . $table_name . " (
         id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
         post_id BIGINT(20) NOT NULL DEFAULT 0,
@@ -120,15 +136,46 @@ function social_referrals_install() {
         twitter INT(11) NOT NULL DEFAULT 0,
         ts TIMESTAMP NOT NULL,
         UNIQUE KEY id (id));";
-    
+
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
-    
+
     $social_referrals_db_ver = 1.0;
     add_option("social_referrals_db_ver", $social_referrals_db_ver);
-    
+
 }
 
 register_activation_hook(__FILE__, 'social_referrals_install');
+
+function social_referrals_db_update() {
+    global $wpdb;
+
+    $curr_social_referrals_ver = 1.1;
+    $social_referrals_db_ver = get_option("social_referrals_db_ver", 0);
+
+    if ($social_referrals_db_ver == 0) { social_referrals_install(); }
+    $social_referrals_db_ver = get_option("social_referrals_db_ver", 0);
+
+    if ($social_referrals_db_ver < 1.1) {
+        $table_name = $wpdb->prefix . "social_referrals";
+
+        $sql = "CREATE TABLE " . $table_name . " (
+            id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
+            post_id BIGINT(20) NOT NULL DEFAULT 0,
+            referral_url TEXT NOT NULL DEFAULT '',
+            facebook INT(11) NOT NULL DEFAULT 0,
+            twitter INT(11) NOT NULL DEFAULT 0,
+            wordpress INT(11) NOT NULL DEFAULT 0,
+            ts TIMESTAMP NOT NULL,
+            UNIQUE KEY id (id));";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        $social_referrals_db_ver = 1.1;
+        add_option("social_referrals_db_ver", $social_referrals_db_ver);
+    }
+}
+
+//add_action( 'plugins_loaded', 'social_referrals_db_update' );
 
 ?>
